4d522dd8528b8b00c68e25917726e7511094068a
AvrahamBi <avrahambarilan@gmail.com>
2021-11-18 15:26:13 +0200

    Some lines added

--- ex2.py
+++ ex2.py
@@ -1,11 +1,25 @@
-A
+import sys
+import numpy as np
+
+# output of functions is array of predictions
+def knn():
+    # python ex2.py <train_x_path> <train_y_path> <test_x_path> <output_log_name>
+    train_x_path, train_y_path, test_x_path, output_log_name = sys.argv[1], sys.argv[2],sys.argv[3],sys.argv[4]
+    print("Check")
 
 
-def main():
-    print("Hello World!")
 
 
 
+
+def perceptron():
+    pass
+
+def svm():
+    pass
+
+def pa():
+    pass
     
 if __name__ == "__main__":
-    main()
+    knn()
