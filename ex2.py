1e3f9325000f86ddf7ee7a817b0d1f0bf7b3b533
AvrahamBi <avrahambarilan@gmail.com>
2021-11-19 13:25:36 +0200

    Few functions added

--- ex2.py
+++ ex2.py
@@ -1,10 +1,35 @@
 import sys
 import numpy as np
 
+
+def convertListToFloat(txtPoints):
+    points = []
+    for txtPoint in txtPoints:
+        point = []
+        point.clear()
+        for val in txtPoint:
+            point.append(float(val))
+        points.append(point)
+    return points
+
+def dist(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    return np.linalg.norm(a - b)
+
 # output of functions is array of predictions
 def knn():
     # python ex2.py <train_x_path> <train_y_path> <test_x_path> <output_log_name>
     train_x_path, train_y_path, test_x_path, output_log_name = sys.argv[1], sys.argv[2],sys.argv[3],sys.argv[4]
+    # open file
+    with open(train_x_path) as file:
+        txtPoints = file.readlines()
+        txtPoints = [line.rstrip().split(",") for line in txtPoints]
+    file.close()
+    # convert to float
+    points = convertListToFloat(txtPoints)
+
+
     print("Check")
 
 
