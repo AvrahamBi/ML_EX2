a0a2ca27856264c795d1b37d2859f8d6101c972a
AvrahamBi <avrahambarilan@gmail.com>
2021-11-29 23:39:51 +0200

    svm done

--- ex2.py
+++ ex2.py
@@ -85,11 +85,59 @@ def perceptron(X, Y, lr, numOfEpocs, test):
     return predictions
 
 
-def svm():
-    pass
+def svm(X, Y, lr, numOfEpocs, test, delta):
+    n_samples, n_features = X.shape
+    w = np.zeros((3, X.shape[1]))
+    for x in range(numOfEpocs):
+        for x_i, y_i in zip(X, Y):
+            y_hat = np.argmax(np.dot(w, x_i))
+            y_i = int(y_i)
+            y_hat = int(y_hat)
+            loss = 1 - np.dot(x_i, w[y_i]) + np.dot(x_i, w[y_hat])
+            if 0 < loss:
+                deltaLr = 1 - (delta * lr)
+                lrUpdate = lr * x_i
+                w[y_i] = deltaLr * w[y_i] + lrUpdate
+                w[y_hat] = deltaLr * w[y_hat] - lrUpdate
+                for i in range(len(w)):
+                    if i != y_i and i != y_hat:
+                        w[i] = w[i] * deltaLr
+            else:
+                w = w * (1 - delta * lr)
+        if x % 10 == 1:
+            lr *= 0.1
+    predictions = []
+    for sample in test:
+        y_hat = np.argmax(np.dot(w, sample))
+        predictions.append(int(y_hat))
+    return predictions
 
-def pa():
-    pass
+def pa(X, Y, lr, numOfEpocs, test, delta):
+    n_samples, n_features = X.shape
+    w = np.zeros((3, X.shape[1]))
+    for x in range(numOfEpocs):
+        for x_i, y_i in zip(X, Y):
+            y_hat = np.argmax(np.dot(w, x_i))
+            y_i = int(y_i)
+            y_hat = int(y_hat)
+            loss = 1 - np.dot(x_i, w[y_i]) + np.dot(x_i, w[y_hat])
+            if 0 < loss:
+                deltaLr = 1 - (delta * lr)
+                lrUpdate = lr * x_i
+                w[y_i] = deltaLr * w[y_i] + lrUpdate
+                w[y_hat] = deltaLr * w[y_hat] - lrUpdate
+                for i in range(len(w)):
+                    if i != y_i and i != y_hat:
+                        w[i] = w[i] * deltaLr
+            else:
+                w = w * (1 - delta * lr)
+        if x % 10 == 1:
+            lr *= 0.1
+    predictions = []
+    for sample in test:
+        y_hat = np.argmax(np.dot(w, sample))
+        predictions.append(int(y_hat))
+    return predictions
 
 if __name__ == "__main__":
     trainPoints = np.loadtxt(sys.argv[1], delimiter=",")
@@ -99,11 +147,10 @@ if __name__ == "__main__":
     trainPoints = zscore_normalization(trainPoints)
     testPoints = zscore_normalization(testPoints)
 
-    compare(perceptron(trainPoints, resultsVector, 0.1, 30, trainPoints))
+    compare(perceptron(trainPoints, resultsVector, 0.3, 30, trainPoints))
 
-    #knnPrediction = knn(3)
+    #knnVector = knn(3)
+    #perceptronVector = perceptron(trainPoints, resultsVector, 0.3, 30, trainPoints)
+    #svmVector = perceptron(trainPoints, resultsVector, 0.3, 30, trainPoints, 0.01)
 
-    #perceptronPrediction = perceptron()
-    #svmPrediction = svm()
-    #paPrediction = pa()
     # output_file = open(sys.argv[4], "w")
